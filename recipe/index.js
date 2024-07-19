425706a8c2b0ecdbfb82e2ebf63846b88e5a2777
Nia Wright <nw.niawright@gmail.com>
2024-07-19 14:45:28 -0400

    Added index.js file

---
+++ recipe/index.js
@@ -0,0 +1,7 @@
+// DOM manipulation
+/* Objectives
+    - footer appears when user scrolls down
+    - footer disappears when user scrolls up
+    - modify opacity when user hovers over links
+    - add checkboxes after li with class steps
+*/
