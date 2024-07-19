605382a3da5b3d514e90f72f329c270f6f959ec4
Nia Wright <nw.niawright@gmail.com>
2024-07-19 17:44:15 -0400

    Modifies opacity when user hovers over links

--- recipe/index.js
+++ recipe/index.js
@@ -5,3 +5,45 @@
     - modify opacity when user hovers over links
     - add checkboxes after li with class steps
 */
+
+// const myListItem = document.getElementsByClassName("step");
+// const desc = document.getElementById("description");
+
+// let checkbox = document.createElement("input");
+
+// checkbox.type = "checkbox";
+// checkbox.name = "name";
+// checkbox.value = "value";
+// checkbox.id = "check";
+
+// myListItem.innerHTML = "HELLO";
+// desc.append(checkbox);
+
+// const allLinks = document.getElementsByClassName("hoveredLink");
+// allLinks.addEventListener("hover", function() {
+//     document.a.style.color = "red";
+// });
+
+const listItem1 = document.getElementById("hoveredLink1");
+listItem1.addEventListener("mouseover", function() {
+    listItem1.style.opacity = "60%";
+}, false);
+listItem1.addEventListener("mouseout", function() {
+    listItem1.style.opacity = "100%";
+}, false)
+
+const listItem2 = document.getElementById("hoveredLink2");
+listItem2.addEventListener("mouseover", function() {
+    listItem2.style.opacity = "60%";
+}, false);
+listItem2.addEventListener("mouseout", function() {
+    listItem2.style.opacity = "100%";
+}, false)
+
+const listItem3 = document.getElementById("hoveredLink3");
+listItem3.addEventListener("mouseover", function() {
+    listItem3.style.opacity = "60%";
+}, false);
+listItem3.addEventListener("mouseout", function() {
+        listItem3.style.opacity = "100%";
+}, false)
