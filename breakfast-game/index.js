edffd0b53bfa0e5a8038d1fa2f61c6f2f1ff6d20
Nia Wright <nw.niawright@gmail.com>
2024-08-06 16:56:09 -0400

    Add more pseudocode and scaffolding to map out project

--- breakfast-game/index.js
+++ breakfast-game/index.js
@@ -5,3 +5,19 @@
 //         - the percentage of waffle / pancake win should be 50 / 50
 //     4. show the result of the coin toss by displaying either waffle or pancake
 //     5. include a replay or start over button so user can keep tossing
+
+// function to handle user click
+function userClick() {
+    // define var chosenside
+    let chosenSide = "";
+    // use random function to pick a side with randint(0, 1)
+    // define what displays when 0 or 1 is chosen
+    // if 0 then show waffle, if 1 then show pancake
+
+    // return chosenSide which is either 'waffle' or 'pancake'
+}
+
+// function gameRestart to handle user clicking restart button
+function gameRestart() {
+    // shows starting breakfast div and hides waffle & pancake div
+}
