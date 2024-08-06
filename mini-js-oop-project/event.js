1b7afcfe8060722e22d430e498838348d0854c75
Nia Wright <nw.niawright@gmail.com>
2024-08-06 18:29:14 -0400

    Add more whitespace to js file to improve readability

--- mini-js-oop-project/event.js
+++ mini-js-oop-project/event.js
@@ -22,26 +22,36 @@ class Event {
 
     // --- 14, 15 ---
     allTickets() {
-        return "All Tickets: " + this.availableTickets.map((ticket, index) => `${index + 1}. ${ticket.name} ($${ticket.price})`).join(" ");
+        return "All Tickets: " + this.availableTickets.map((ticket, index) => 
+            `${index + 1}. ${ticket.name} ($${ticket.price})`).join(" ");
     }
 
     // --- 16 ---
     searchTickets(lower, upper) {
-        const eligibleTickets = this.availableTickets.filter(ticket => ticket.price >= lower && ticket.price <= upper);
+        const eligibleTickets = this.availableTickets.filter(ticket => 
+            ticket.price >= lower && ticket.price <= upper);
+
         if (eligibleTickets.length === 0) {
             return "No tickets available";
         }
-        return `Eligible Tickets (<em>$${lower} - $${upper}</em>): ` + eligibleTickets.map((ticket, index) => `${index + 1}. ${ticket.ticketName} ($${ticket.price})`).join(" ")
+
+        return `Eligible Tickets (<em>$${lower} - $${upper}</em>): ` + 
+        eligibleTickets.map((ticket, index) => 
+            `${index + 1}. ${ticket.ticketName} ($${ticket.price})`).join(" ");
     }
 
     getCheapestTicket() {
         if (this.availableTickets.length === 0) {
             return "No tickets available"
         }
-        const cheapestTicket = this.availableTickets.reduce((min, ticket) => ticket.price < min.price ? ticket : min);
+
+        const cheapestTicket = this.availableTickets.reduce((min, ticket) => 
+            ticket.price < min.price ? ticket : min);
+
         return `<strong>Cheapest Ticket:</strong> ${cheapestTicket.ticketName} ($${cheapestTicket.price})`;
     }
 }
+
 
 // --- 4, 5 ---
 const eventObj1 = new Event(
@@ -57,8 +67,10 @@ const eventObj3 = new Event(
     "The final round of the sprint events for men and women"
 );
 
+
 // --- 6 ---
 const eventArray = new Array();
+
 
 // --- 7, 8 ---
 eventArray.push(eventObj1);
@@ -66,9 +78,11 @@ eventArray.push(eventObj1);
 eventArray.push(eventObj2, eventObj3);
 // console.log(eventArray);
 
+
 // --- 12 ---
 eventObj1.addAvailableTickets("General Admission", 99);
 eventObj1.addAvailableTickets("Front Row", 299);
+
 
 // --- 13 ---
 eventObj2.addAvailableTickets("Upper Tier Seating", 25);
@@ -77,6 +91,7 @@ eventObj2.addAvailableTickets("Lower Tier Seating", 80);
 eventObj3.addAvailableTickets("VIP Box", 300);
 eventObj3.addAvailableTickets("Lower Tier Seating", 200);
 eventObj3.addAvailableTickets("Upper Tier Seating", 100);
+
 
 // --- 9 ---
 // document.addEventListener('DOMContentLoaded', () => {
@@ -93,23 +108,30 @@ eventObj3.addAvailableTickets("Upper Tier Seating", 100);
 
 document.addEventListener('DOMContentLoaded', () => {
     let html = '';
+
     eventArray.forEach((event) => {
         html += `<li> <strong>${event.name}</strong> - ${event.description} <br/> ${event.searchTickets(0, 300)}</li>`
-    })
+    });
+
     document.querySelector("#event").innerHTML = html
 })
+
 
 // --- CHALLENGE ---
 document.addEventListener('DOMContentLoaded', () => {
     let html = '';
+
     eventArray.forEach((event) => {
         html += `<li> ${event.name} - ${event.description} <br/> ${event.searchTickets(0, 100)} <br/> ${event.getCheapestTicket()}</li>`
-    })
+    });
+
     document.querySelector("#cheapTicketContainer").innerHTML = html;
 })
 
+
 // test cases for searchTicket
-console.log("Test case 1: ", eventObj3.searchTickets(0, 250));
+console.log("Test case 1: ", eventObj3.searchTickets(0, 250)); // returns eligible tickets priced $0-$250
+
 
 // test case for cheapest Ticket
 console.log("Event 1: ", eventObj1.getCheapestTicket()); // returns cheapest ticket
