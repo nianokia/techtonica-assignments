5a6d1dbf9c1174a6954af30ae75b6ba509667ff4
Nia Wright <nw.niawright@gmail.com>
2024-08-17 00:46:33 -0400

    Import and call all components to App.jsx

--- react-game/src/App.jsx
+++ react-game/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './App.css'
 
+import Title from './Title'
+import PickASide from './PickASide'
+import CoinFlip from './CoinFlip'
+import Restart from './Restart'
+import ScoreBoard from './ScoreBoard'
+import Outcome from './Outcome'
+
 export default function App() {
 
   return (
-    <>
-      <h1>React Game</h1>
-    </>
+    <div className='App'>
+      <Title />
+      <PickASide />
+      <CoinFlip />
+      <Restart />
+      <ScoreBoard />
+      <Outcome />
+    </div>
   )
 }
 
