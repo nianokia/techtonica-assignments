13a61be1565ded50b909a29f9469ddb74a28d085
Nia Wright <nw.niawright@gmail.com>
2024-08-19 12:43:24 -0400

    Set outcome state and display outcome inside summary

--- react-game/src/Outcome.jsx
+++ react-game/src/Outcome.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Outcome() {
+export default function Outcome(props) {
+    // set Outcome
+    const [outcome, setOutcome] = useState("");
+
+    // set outcome message based off winner
+    if (props.winner === "user") {
+        setOutcome("You won!");
+    } else {
+        setOutcome("You lost.");
+    }
+
     return (
         <div className="Outcome">
-            <summary></summary>
+            <summary>{outcome}</summary>
         </div>
     )
 }
