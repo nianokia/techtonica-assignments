00ebcf02e02c3c0c15d146b85f851fd8405417ae
Nia Wright <nw.niawright@gmail.com>
2024-08-19 12:58:46 -0400

    Make Restart button reload the page

--- react-game/src/Restart.jsx
+++ react-game/src/Restart.jsx
@@ -4,9 +4,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function Restart() {
+    const handleRestart = (event) => {
+        event.preventDefault();
+        window.location.reload();
+    }
     return (
         <div className="Restart">
-            <button id="restartButton">
+            <button id="restartButton" onClick={handleRestart}>
                 <FontAwesomeIcon icon={faRotateRight} />
             </button>
         </div>
