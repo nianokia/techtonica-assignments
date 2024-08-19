6c8a88aa7b65db624a870b7839f00def229cb9bc
Nia Wright <nw.niawright@gmail.com>
2024-08-19 12:32:17 -0400

    Move components out of App into PickASide in order to pass props

--- react-game/src/App.jsx
+++ react-game/src/App.jsx
@@ -3,10 +3,6 @@ import './App.css'
 
 import Title from './Title'
 import PickASide from './PickASide'
-import CoinFlip from './CoinFlip'
-import Restart from './Restart'
-import ScoreBoard from './ScoreBoard'
-import Outcome from './Outcome'
 
 export default function App() {
 
@@ -14,10 +10,6 @@ export default function App() {
     <div className='App'>
       <Title />
       <PickASide />
-      <CoinFlip />
-      <Restart />
-      <ScoreBoard />
-      <Outcome />
     </div>
   )
 }
