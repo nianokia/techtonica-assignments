71e2bb735512824a7b2511cae7bf41f3b2fd15eb
Nia Wright <nw.niawright@gmail.com>
2024-08-19 12:42:33 -0400

    Define winner and score states, set winner and score, pass props to Outcome

--- react-game/src/ScoreBoard.jsx
+++ react-game/src/ScoreBoard.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Outcome from "./Outcome";
 
-export default function ScoreBoard() {
+export default function ScoreBoard(props) {
+    // setWinner, setUserScore, setCompScore
+    const [winner, setWinner] = useState(null);
+    const [userScore, setUserScore] = useState(0);
+    const [compScore, setCompScore] = useState(0);
+
+    const handleWinner = () => {
+        if (props.userChoice === props.randomSide) {
+            setUserScore(userScore + 1);
+            setWinner("user");
+        } else {
+            setCompScore(compScore + 1);
+            setWinner("computer");
+        }
+    }
+
+    handleWinner;
+
     return (
         <div className="ScoreBoard">
             <span className="userScoreBoard">
                 <h3>User Score</h3>
-                <p id="userScore">0</p>
+                <p id="userScore">{userScore}</p>
             </span>
             <span className="compScoreBoard">
                 <h3>Computer Score</h3>
-                <p id="compScore">0</p>
+                <p id="compScore">{compScore}</p>
             </span>
+            <Outcome winner={winner}/>
         </div>
     )
 }
