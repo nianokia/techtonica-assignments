48ae2dd489728f110be071745682137b7b69fecc
Nia Wright <nw.niawright@gmail.com>
2024-08-20 15:54:14 -0400

    Implement review and remove commented-out code to reduce clutter

--- react-game/src/Outcome.jsx
+++ react-game/src/Outcome.jsx
@@ -1,20 +1,6 @@
 import React from "react";
 
 export default function Outcome(props) {
-    // set outcome message based off winner
-    // FOR SOME REASON THIS CODE BLOCK IS BREAKING THE PAGE
-    // if (props.winner === "user") {
-    //     setOutcome("You won!");
-    // } else {
-    //     setOutcome("You lost.");
-    // }
-
-    // if (props.isGameOver == true && props.winner == "user") {
-    //     "Hooray you won!";
-    // } else {
-    //     "Sorry, no breakfast for you.";
-    // }
-
     return (
         <div className="Outcome">
             <summary>
