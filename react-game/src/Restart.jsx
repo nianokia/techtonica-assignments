7302c123a9785e29338b707f3a69fac856700bdd
Nia Wright <nw.niawright@gmail.com>
2024-08-20 15:39:36 -0400

    Change functionality to reset states rather than refresh the page window

--- react-game/src/Restart.jsx
+++ react-game/src/Restart.jsx
@@ -3,10 +3,16 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
-export default function Restart() {
+export default function Restart({setUserChoice, setUserScore, setCompScore, setMoves, setImage, breakfast}) {
     const handleRestart = (event) => {
         event.preventDefault();
-        window.location.reload();
+
+        // reset all states from PickASide component
+        setUserChoice(null);
+        setUserScore(0);
+        setCompScore(0);
+        setMoves(3);
+        setImage(breakfast);
     }
     return (
         <div className="Restart">
