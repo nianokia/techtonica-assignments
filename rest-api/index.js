1ee622bc826f9b901a8f37aefe3e2093a8046f4c
Nia Wright <nw.niawright@gmail.com>
2024-08-21 15:10:07 -0400

    Create homepage and page that show the manga data in JSON format

--- rest-api/index.js
+++ rest-api/index.js
@@ -11,9 +11,13 @@ app.use(cors());
 
 // create route for CREATE (get)
 app.get('/', (req, res) => {
-    res.json(manga);
+    res.json("Welcome to my REST API");
     console.log("SUCCESS!");
-})
+});
+
+app.get('/manga', (req, res) => {
+    res.json(manga);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
