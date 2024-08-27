d138fbb5ff9721d6bfc9e231b0950af838f0e321
Nia Wright <nw.niawright@gmail.com>
2024-08-27 17:12:26 -0400

    Add .env file and store in .gitignore, Import dotenv package to server

--- weather-project/server.js
+++ weather-project/server.js
@@ -1,11 +1,17 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
+const port = 3000;
+import 'dotenv/config';
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.get(('/weather', (req, res) => {
+    const city = req.query.cityNmae;
+    const apiKey = process.env.APIKEY;
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
