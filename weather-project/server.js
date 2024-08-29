d12b98d36f37fec95fd1dbb77431b65c8ee23740
Nia Wright <nw.niawright@gmail.com>
2024-08-29 19:22:29 -0400

    Import express and fetch data from API for input city

--- weather-project/server.js
+++ weather-project/server.js
@@ -1,31 +1,33 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import cors from 'cors';
 import 'dotenv/config';
+
+const app = express();
+const port = 3030;
+
+// have express app use middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get(('/weather', (req, res) => {
-    const city = req.query.cityNmae;
-    const apiKey = process.env.APIKEY;
-    const params = new URLSearchParams({
-        q: req.query.cityName,
-        appid: process.env.APIKEY,
-        units: "imperial"
-    });
-    const url = `https://api.openweathermap.org/data/2.5/weather?${params}`;
-    console.log(url);
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            res.send({ data });
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-}));
+app.get('/weather', async (req, res) => {
+  const city = req.query.cityName;
+  const apiKey = process.env.APIKEY;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  await fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data });
+    })
+    .catch((err) => {
+      console.log(err);
+    }
+  )
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`)
