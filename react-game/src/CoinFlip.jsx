de2321e28ca6314762eaf0f11a3bec16065441f6
Xiahui Zhang <xhz0729@gmail.com>
2024-09-05 14:42:10 -0500

    changing the CoinFlip component

--- react-game/src/CoinFlip.jsx
+++ react-game/src/CoinFlip.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './CoinFlip.css'
 
 export default function CoinFlip(props) {
     return (
         <div className="CoinFlip">
-            <img id="coin" src={props.image} />
+            <img id="coin" 
+            src={props.image} 
+            alt="coin"
+            className={props.isFlipping ? (props.side === 'waffle' ? 'heads-flip' : 'tails-flip') : ''}
+            />
         </div>
     )
 }
