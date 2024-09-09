7538754af38a5f8dd0164cf67bde26ec1add8ffa
Nia Wright <nw.niawright@gmail.com>
2024-09-08 23:34:44 -0400

    Handle input change and store user answer to each question. Map through questions, then map through answers

--- full-stack-game/client/src/TriviaForm.jsx
+++ full-stack-game/client/src/TriviaForm.jsx
@@ -2,14 +2,15 @@
 PSEUDOCODE:
 
 Requirements:
-- Gather quiz data (√)
-- Seperate each question object
-- Show all 5 question and answer choices
-- Set userAnswers to be the event.target.value of each question
-- Collect # of answered questions
-- Monitor # questions user got right/wrong
-- Display game outcome message whether they won or lost
+- Gather quiz data ( √ )
+- Separate each question object ( √ )
+- Show all 5 question and answer choices ( √ )
+- Set userAnswers to be the event.target.value of each question (  )
+- Collect # of answered questions (  )
+- Monitor # questions user got right/wrong (  )
+- Display game outcome message whether they won or lost (  )
     - if user got majority right they won (score >= 3), if not they lost (score <= 2)
+    - maybe use a switch for each score
 
 Nice-to-Haves
 - Reset Button to restart the Quiz
@@ -29,115 +30,80 @@ Future Implementations
 */
 
 import React, { useEffect, useState } from "react";
+import mockTrivia from './mockTrivia.json'
 
 export default function TriviaForm() {
-    const [quiz, setQuiz] = useState([]);
-    const [answers, setAnswers] = useState({});
+  const [quiz, setQuiz] = useState([]);
+  const [userAnswer, setUserAnswer] = useState({});
+  const [score, setScore] = useState(0);
+  const mock = mockTrivia.data.results;
 
-    useEffect(() => {
-        const retrieveQuizData = async () => {
-            try {
-                const response = await fetch(`http://localhost:4070/trivia`);
-                const quizData = await response.json();
-                console.log(quizData);
-                setQuiz(quizData.data.results);
-            } catch (err) {
-                console.log("Error:", err);
-            }
-        };
-        retrieveQuizData();
-        // const submitAnswers = async () => {
-        //     fetch(`http://localhost:4070/trivia`)
-        //         .then((response) => response.json())
-        //         .then((result) => {
-        //             let quizData = result.data.results;
-        //             console.log(quizData);
-        //             setQuiz({
-        //                 isLoaded: true,
-        //                 question1: {
-        //                     question: quizData[0].question,
-        //                     correct: quizData[0].correct_answer,
-        //                     incorrect1: quizData[0].incorrect_answers[0],
-        //                     incorrect2: quizData[0].incorrect_answers[1],
-        //                     incorrect3: quizData[0].incorrect_answers[2]
-        //                 },
-        //                 question2: {
-        //                     question: quizData[1].question,
-        //                     correct: quizData[1].correct_answer,
-        //                     incorrect1: quizData[1].incorrect_answers[0],
-        //                     incorrect2: quizData[1].incorrect_answers[1],
-        //                     incorrect3: quizData[1].incorrect_answers[2]
-        //                 },
-        //                 question3: {
-        //                     question: quizData[2].question,
-        //                     correct: quizData[2].correct_answer,
-        //                     incorrect1: quizData[2].incorrect_answers[0],
-        //                     incorrect2: quizData[2].incorrect_answers[1],
-        //                     incorrect3: quizData[2].incorrect_answers[2]
-        //                 },
-        //                 question4: {
-        //                     question: quizData[3].question,
-        //                     correct: quizData[3].correct_answer,
-        //                     incorrect1: quizData[3].incorrect_answers[0],
-        //                     incorrect2: quizData[3].incorrect_answers[1],
-        //                     incorrect3: quizData[3].incorrect_answers[2]
-        //                 },
-        //                 question5: {
-        //                     question: quizData[4].question,
-        //                     correct: quizData[4].correct_answer,
-        //                     incorrect1: quizData[4].incorrect_answers[0],
-        //                     incorrect2: quizData[4].incorrect_answers[1],
-        //                     incorrect3: quizData[4].incorrect_answers[2]
-        //                 },
-        //             });
-        //         }
-        //     );
-        // };  
-    }, []);
-    
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // submitAnswers();
+  useEffect(() => {
+    const retrieveQuizData = async () => {
+      try {
+          const response = await fetch(`http://localhost:4070/trivia`);
+          const quizData = await response.json();
+          setQuiz(quizData.data.results);
+      } catch (err) {
+          console.log("Error:", err);
+      }
     };
+    retrieveQuizData();
+  }, []);
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const mergeAnswers = (correct, incorrect) => {
-        incorrect.push(correct);
+    
+  };
+
+  const mergeAnswers = (correct, incorrect) => {
+    let allAnswers = incorrect.slice();
+    allAnswers.push(correct);
+    return allAnswers;
+  }
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    let userAnswersArray = {
+      "question-0": "",
+      "question-1": "",
+      "question-2": "",
+      "question-3": "",
+      "question-4": ""
     }
+    userAnswersArray = event.target.value;
+    console.log(userAnswersArray);
+  }
 
-    return (
-        <div className="TriviaForm">
-            <form onSubmit={handleSubmit}>
-                <section className="questionBlock" style={{textAlign: "start"}}>
-                    <br/>
-                    {quiz.map((question) => {
-                        
-                        return (
-                            <>
-                                Question: {question.question}
-                                <br />
-                                {question.incorrect_answers.map((answer) => {
-                                    return (
-                                        <>
-                                            <label><input type="radio" name="answerChoice" value="answer" />{answer}</label>
-                                            <br/>
-                                        </>   
-                                    )
-                                })}
-                            </> 
-                        )  
-                    })}
-                    
-                    {/* <label><input type="radio" name="answerChoice" value="answer1" />Answer 1</label>
-                    <br/>
-                    <label><input type="radio" name="answerChoice" value="answer2" />Answer 2</label>
-                    <br/>
-                    <label><input type="radio" name="answerChoice" value="answer3" />Answer 3</label>
-                    <br/>
-                    <label><input type="radio" name="answerChoice" value="answer4" />Answer 4</label> */}
-                </section>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="TriviaForm">
+      <form onSubmit={handleSubmit}>
+        <section className="questionBlock" style={{textAlign: "start"}}>
+          {/* Remember to change mock.map below back to quiz.map when API restores calls */}
+          {mock.map((question, questionIndex) => {
+            let answers = mergeAnswers(question.correct_answer, question.incorrect_answers);
+            return (
+              <fieldset id={`question-${questionIndex}`} key={`question-${questionIndex}`}>
+                Question: {question.question}
+                <br />
+                {answers.map((answer, answerIndex) => {
+                  return (
+                    <div key={`answer-${answerIndex}`}>
+                      <label>
+                        <input type="radio" name={`question-${questionIndex}`} value={answer} onChange={handleInputChange} />
+                        {answer}
+                        </label>
+                      <br/>
+                    </div>   
+                  )
+                })}
+              </fieldset> 
+            )  
+          })}
+        </section>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
