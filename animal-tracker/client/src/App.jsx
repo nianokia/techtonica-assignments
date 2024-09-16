ce953017356ebb2bc7e5a8c9c140f79a17b0179e
Nia Wright <nw.niawright@gmail.com>
2024-09-16 01:33:57 -0400

    Add createContext inside context folder in Context.js file. Import to App.jsx to conditionally show forms

--- animal-tracker/client/src/App.jsx
+++ animal-tracker/client/src/App.jsx
@@ -1,16 +1,30 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListSightings from './components/ListSightings'
+import MyNavBar from './components/Navbar';
+import ListSightings from './components/ListSightings';
+import SightingsForm from './components/SightingsForm.jsx';
+import IndividualForm from './components/IndividualForm';
+import SpeciesForm from './components/SpeciesForm.jsx';
+import { AllContext } from './context/Context.js';
 
 
 function App() {
+  const [form, setForm] = useState("sightings");
 
   return (
     <div className="App">
       <MyNavBar />
       <ListSightings />
 
+      <AllContext.Provider value={{
+        form,
+        setForm
+      }}>
+        {/* {form === "sightings" && <SightingsForm />} */}
+        {form === "individual" && <IndividualForm />}
+        {form === "species" && <SpeciesForm />}
+      </AllContext.Provider>
     </div>
   )
 }
