14bd68efb9e1dca40ec62b97f892be53c2379d45
Nia Wright <nw.niawright@gmail.com>
2024-09-16 00:08:26 -0400

    Add data from multiple tables using JOIN method to each Card. Add function to format the date to be easier to read

--- animal-tracker/client/src/components/Sighting.jsx
+++ animal-tracker/client/src/components/Sighting.jsx
@@ -13,11 +13,32 @@ const Sighting = ({sighting, toUpdate, toDelete}) => {
         toDelete(toDeleteSighting)
     }
 
+    const formatDate = (date_time) => {
+        let date = new Date(date_time);
+        // new Date().toJSON().slice(0, 16);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        let hour = date.getHours();
+        if (hour < 10) {
+            hour = "0" + hour;
+        }
+        let minutes = date.getMinutes();
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        // 2024-09-11 22:30:21.231986
+        return `${month}/${day}/${year} ${hour}:${minutes}`;
+    }
+
     return (
         <Card>
             <Card.Body>
-            <Card.Title>{sighting.individual}</Card.Title>
-            <Card.Text>{sighting.date_time}</Card.Text>
+            <Card.Title>{sighting.individual} - {sighting.species}</Card.Title>
+            <Card.Text>{formatDate(sighting.date_time)}</Card.Text>
+            <Card.Text>{sighting.location}</Card.Text>
+            <Card.Text>{sighting.health} {sighting.status_code}</Card.Text>
             <Button variant="outline-danger" onClick={()=>{onDelete(sighting)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(sighting)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
             </Card.Body>
