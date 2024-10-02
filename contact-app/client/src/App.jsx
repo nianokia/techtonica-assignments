84810358eee59556c5298385a1c48e91d0076587
Nia Wright <nw.niawright@gmail.com>
2024-10-02 15:31:12 -0400

    Add basic pseudocode

--- contact-app/client/src/App.jsx
+++ contact-app/client/src/App.jsx
@@ -1,6 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 
+/*
+PSEUDOCODE:
+- ---- HOMEPAGE ----
+  - add contact button (comp)
+    - present a form onClick
+      - inputs = name (text), email (email) , phone (text) [validate w/ regex], notes (text)
+        - all required except notes
+  - list of contacts (comp) [ul then map through contacts in li]
+    - contact (comp)
+      - edit contact button
+      - delete contact button
+
+- ---- SERVER ----
+  - GET route - retrieve all contacts (SELECT * FROM contacts)
+  - POST route - add a contact (INSERT INTO contacts (name, email, phone, notes) VALUES ('', '', '', ''))
+  - PUT route - edit specific contact_id ()
+  - DELETE route - delete specific contact_id (DELETE contact_id FROM contacts)
+*/
+
 function App() {
   const [count, setCount] = useState(0)
 
